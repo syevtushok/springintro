f9c87ccbc56b2ea4a0ca6cc69a37de6eaedfcd28
ggazila <ggazila@gmail.com>
2020-02-13 13:49:57 +0200

    configurate db.properties (#2)

--- src/main/java/spring/intro/config/AppConfig.java
+++ src/main/java/spring/intro/config/AppConfig.java
@@ -8,23 +8,33 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import spring.intro.models.User;
 
 @Configuration
+@PropertySource("classpath:db.properties")
 @ComponentScan(basePackages = {"spring.intro.dao",
         "spring.intro.services"})
 public class AppConfig {
+    private final Environment environment;
+
+    public AppConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public LocalSessionFactoryBean getSessionFactory() {
         LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
         factoryBean.setDataSource(getDataSource());
 
         Properties properties = new Properties();
-        properties.put("show_sql", true);
-        properties.put("format_sql", true);
-        properties.put("use_sql_comments", true);
-        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        properties.put("show_sql", environment.getProperty("show_sql"));
+        properties.put("format_sql", environment.getProperty("format_sql"));
+        properties.put("use_sql_comments", environment.getProperty("use_sql_comments"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl"
+                                                                         + ".auto"));
         factoryBean.setHibernateProperties(properties);
         factoryBean.setAnnotatedClasses(User.class);
         return factoryBean;
@@ -33,10 +43,10 @@ public class AppConfig {
     @Bean
     public DataSource getDataSource() {
         BasicDataSource source = new BasicDataSource();
-        source.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        source.setUrl("jdbc:mysql://localhost/springintro?serverTimezone=UTC");
-        source.setUsername("admin");
-        source.setPassword("admin");
+        source.setDriverClassName(environment.getProperty("db.driver"));
+        source.setUrl(environment.getProperty("db.url"));
+        source.setUsername(environment.getProperty("db.username"));
+        source.setPassword(environment.getProperty("db.password"));
         return source;
     }
 }
